ea538f61d4860ba6b3bd08337f559c8cece23350
vincent <vincentwardhana33@gmail.com>
2020-10-06 20:29:35 +0700

    step 30 slide 36

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './types';
 
 export const emailChanged = (text) => {
     return {
@@ -19,10 +19,27 @@ export const loginUser = ({ email, password }) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => {
-            dispatch({
-                type: LOGIN_USER_SUCCESS,
-                payload: user
-            });
+            loginUserSuccess(dispatch, user);
+        })
+        .catch((error) => {
+            console.log(error);
+
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(user => {
+                    loginUserSuccess(dispatch, user);
+                })
+                .catch(() => loginUserFail(dispatch));
         });
     };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+};
+
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL });
 };
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,10 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/types';
 
 const INITIAL_STATE = {
     email: '',
-    password: ''
+    password: '',
+    user: null,
+    error: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -11,6 +13,10 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
+        case LOGIN_USER_SUCCESS:
+            return { ...state, user: action.payload };
+        case LOGIN_USER_FAIL:
+            return { ...state, error: 'Authentication Failed.' };
         default:
             return state;
     }
